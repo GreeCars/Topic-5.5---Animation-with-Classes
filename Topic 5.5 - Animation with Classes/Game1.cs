c0935a918275bb5158542e8b107a0f8afce1bf48
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-12-04 09:39:01 -0500

    Add project files.

---
+++ Topic 5.5 - Animation with Classes/Game1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Topic_5._5___Animation_with_Classes
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Rectangle window;
+
+        Tribble tribble1;
+
+        Tribble tribble2;
+
+        Texture2D tribbleBrownTexture;
+
+        Texture2D tribbleCreamTexture;
+
+        Texture2D tribbleGreyTexture;
+        Rectangle tribbleGreyRect;
+        Vector2 tribbleGreySpeed;
+
+        Texture2D tribbleOrangeTexture;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            this.Window.Title = "Lesson 3 - Animation Part 1";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            window = new Rectangle(0, 0, 800, 500);
+            _graphics.PreferredBackBufferWidth = window.Width;  // set this value to the desired width of your window
+            _graphics.PreferredBackBufferHeight = window.Height;   // set this value to the desired height of your window
+            _graphics.ApplyChanges();
+
+
+            base.Initialize();
+
+            tribble1 = new Tribble(tribbleGreyTexture, new Rectangle(300, 10, 100, 100), new Vector2(2, 0));
+            tribble2 = new Tribble(tribbleOrangeTexture, new Rectangle(300, 10, 100, 100), new Vector2(0, 2));
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            tribbleGreyTexture = Content.Load<Texture2D>("tribbleGrey");
+            tribbleBrownTexture = Content.Load<Texture2D>("tribbleBrown");
+            tribbleCreamTexture = Content.Load<Texture2D>("tribbleCream");
+            tribbleOrangeTexture = Content.Load<Texture2D>("tribbleOrange");
+
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            tribble1.Move(window);
+
+            
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(tribble1.Texture, tribble1.Rectangle, Color.White);
+            _spriteBatch.Draw(tribble2.Texture, tribble2.Rectangle, Color.White);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
+
+  
---
+++ Topic 5.5 - Animation with Classes/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Topic_5._5___Animation_with_Classes.Game1();
+game.Run();
